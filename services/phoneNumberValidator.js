d19fe8dda40f2662322a6190e4279839e82df364
ephrem-adugna <yadugna@gmail.com>
2023-10-13 09:25:15 -0700

    Updated api url

--- services/phoneNumberValidator.js
+++ services/phoneNumberValidator.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 export const validatePhoneNumber = (phoneNumber) => {
-    return axios.post(`http://apilayer.net/api/validate?access_key=${process.env.NEXT_PUBLIC_PHONE_API_KEY}&number=${phoneNumber}&country_code=US& format = 1`);
+    return axios.post(`https://apilayer.net/api/validate?access_key=${process.env.NEXT_PUBLIC_PHONE_API_KEY}&number=${phoneNumber}&country_code=US& format = 1`);
 }
