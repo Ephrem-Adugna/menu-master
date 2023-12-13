a8cd6064fe9cd3ac489a03466127b2a23c3d5e2d
ephrem-adugna <yadugna@gmail.com>
2023-12-13 09:06:09 -0800

    Notification Bug Fix

--- pages/Menu/Order/NotificationButton/NotificationButtonFirebase.js
+++ pages/Menu/Order/NotificationButton/NotificationButtonFirebase.js
@@ -41,7 +41,32 @@ const NotificationButtonFirebase = ({orderId}) => {
     Notification.requestPermission().then((permission) => {
       if (permission === 'granted') {
         console.log('Notification permission granted.');
-      
+       const db = getDatabase();
+  const dbRef = ref(getDatabase());
+    const messaging = getMessaging();
+    getToken(messaging, {vapidKey: "BKy2Rm_Ts8PmYAaHAePkd8ngCIko4o90QkaDzpknv5bSYSXH1iO7o9fVyJ7C1Sl8QwS5aWoS3ddu2mCUZ8m5ZjU"}).then((currentToken) => {
+      if (currentToken) {
+        console.log(currentToken)
+        get(child(dbRef, `orders/${orderId}`)).then((snapshot) => {
+          if (snapshot.exists()) {
+          var order = snapshot.val();
+          const updates = {};
+        updates[`orders/${orderId}`] = {...order, regToken: currentToken};
+        update(ref(db), updates); 
+                    
+          }});
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+    });
       }
     }
       );
